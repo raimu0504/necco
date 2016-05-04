76aed19f53f7dda85726988b41f558aece20c584
RAIMU <raimu@lime.local>
2016-05-04 16:57:49 +0900

    投稿機能の作成完了

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,16 @@ class TweetsController < ApplicationController
   def index
     @tweet=Tweet.all
   end
+  def new
+    @tweet=Tweet.new
+  end
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:image,:text)
+  end
+
 end
